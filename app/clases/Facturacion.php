343d1644c28a2a2114a98b5c74cf6ea528d51bdf
Gustavo Hekel <gustavo.hekel@gmail.com>
2015-04-08 02:25:59 -0300

    8 abr 2015

--- app/clases/Facturacion.php
+++
@@ -1,9 +0,0 @@
-<?php
-interface Facturacion
-{
-    public function procesar($lote , $file_pointer);
-    public function ingresarError($registro , $lote , $error);
-    public function ingresarRegistro();
-    public function armarArray();
-    public function ingresar();
-}
